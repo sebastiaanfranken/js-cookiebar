722bbe7d54a526f170fb92deeef46e2798583638
Sebastiaan Franken <sebastiaan@sebastiaanfranken.nl>
2015-10-24 13:40:23 +0200

    Test instellingen

--- Extension.php
+++ Extension.php
@@ -12,11 +12,20 @@ class Extension extends BaseExtension
     public function initialize() {
         $this->addCss('assets/cookiebar.css');
         $this->addJavascript('assets/cookiebar.js');
+
+        $this->addSnippet('cookiebarconfig', 'getCookiebarPreferences');
     }
 
     public function getName()
     {
         return "cookiebar";
+    }
+
+    public function getCookiebarPreferences()
+    {
+        $html = '<script type="text/javascript">%s</script>';
+
+        return sprintf($html, json_encode($this->config));
     }
 
 }
